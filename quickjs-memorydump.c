870620c14697cf32407b7de1e19c2a3c28dd4174
wanghaiyun <862555625@qq.com>
2022-10-17 20:36:11 +0800

    [cdp] 完成对接devtools dump内存功能
    Signed-off-by: wanghaiyun <862555625@qq.com>

---
+++ quickjs-memorydump.c
@@ -0,0 +1,118 @@
+
+#include "quickjs-memorydump.h"
+#include "quickjs-debugger.h"
+#ifdef CONFIG_INTERPRETERS_QUICKJS_DEBUG
+void CDP_start_memory_tracking(JSRuntime *rt) {
+  getDumpMemoryInfo(rt)->is_started_memory_tracking = 1;
+}
+void CDP_stop_memory_tracking(JSRuntime *rt) {
+  getDumpMemoryInfo(rt)->is_started_memory_tracking = 0;
+}
+
+CDP_memory_str_val CDP_create_obj_name(const char *str, int len) {
+  CDP_memory_str_val name;
+  name.flag = 0;
+  name.name = str;
+  if (len > 0) {
+    name.flag = STORE_VAL(len, 0);
+  }
+  return name;
+}
+
+CDP_memory_str_val CDP_get_obj_name(JSRuntime *rt, JSAtom atom) {
+  CDP_memory_str_val name;
+  name.flag = 0;
+  if (atom) {
+    name.name = JS_AtomToCString(js_debugger_info(rt)->currCtx, atom);
+    name.flag = STORE_VAL(0, 1);
+  } else {
+    name.name = CDP_UNKNOW_DEFAULT_NAME;
+    name.flag = STORE_VAL(0, 0);
+  }
+  return name;
+}
+
+memory_object_id CDP_get_val_id(JSRuntime *rt, const JSValueConst *val) {
+  if(!val){
+    return ++getDumpMemoryInfo(rt)->currnt_val_id;
+  }
+  if (JS_VALUE_HAS_REF_COUNT(*val)) {
+    return (memory_object_id)JS_VALUE_GET_PTR(*val);
+  }
+  return ++getDumpMemoryInfo(rt)->currnt_val_id;
+}
+
+void CDP_add_proxies_obj_child(JSRuntime *rt, memory_object_id parent_id,
+                               memory_object_id child_id,
+                               CDP_memory_str_val *child_name) {
+  getDumpMemoryInfo(rt)->add_memory_object_child_by_id(rt, parent_id, child_id,
+                                                       child_name);
+}
+
+void CDP_get_stats_update_info(JSRuntime *rt, JSGCObjectHeader *h) {
+  int64_t count = 1;
+  int64_t size = 1;
+  memory_object_id id = (memory_object_id)h;
+  CDP_get_GC_obj_count_and_size(rt, h, &count, &size);
+  getDumpMemoryInfo(rt)->GC_obj_change(rt, id, 1, size);
+}
+
+void CDP_rm_GC_obj(JSRuntime *rt, JSGCObjectHeader *h) {
+  memory_object_id id = (memory_object_id)h;
+  getDumpMemoryInfo(rt)->GC_obj_change(rt, id, 0, 0);
+}
+
+Macro_heap_info CDP_get_heap_usage(JSRuntime *rt) {
+  JSMemoryUsage s;
+  Macro_heap_info macro_heap_info;
+  JS_ComputeMemoryUsage(rt, &s);
+  macro_heap_info.totalSize = s.malloc_size;
+  macro_heap_info.usedSize = s.memory_used_size;
+  macro_heap_info.totalCount = s.malloc_count;
+  return macro_heap_info;
+}
+
+char *CDP_int_to_string(int num, char *str, int radix) {
+  char index[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // 索引表
+  unsigned unum; // 存放要转换的整数的绝对值,转换的整数可能是负数
+  int i = 0, j,
+      k; // i用来指示设置字符串相应位，转换之后i其实就是字符串的长度；转换后顺序是逆序的，有正负的情况，k用来指示调整顺序的开始位置;j用来指示调整顺序时的交换。
+
+  // 获取要转换的整数的绝对值
+  if (radix == 10 && num < 0) // 要转换成十进制数并且是负数
+  {
+    unum = (unsigned)-num; // 将num的绝对值赋给unum
+    str[i++] = '-'; // 在字符串最前面设置为'-'号，并且索引加1
+  } else
+    unum = (unsigned)num; // 若是num为正，直接赋值给unum
+
+  // 转换部分，注意转换后是逆序的
+  do {
+    str[i++] =
+        index[unum %
+              (unsigned)
+                  radix]; // 取unum的最后一位，并设置为str对应位，指示索引加1
+    unum /= radix; // unum去掉最后一位
+
+  } while (unum); // 直至unum为0退出循环
+
+  str[i] = '\0'; // 在字符串最后添加'\0'字符，c语言字符串以'\0'结束。
+
+  // 将顺序调整过来
+  if (str[0] == '-')
+    k = 1; // 如果是负数，符号不用调整，从符号后面开始调整
+  else
+    k = 0; // 不是负数，全部都要调整
+
+  char temp; // 临时变量，交换两个值时用到
+  for (j = k; j <= (i - 1) / 2;
+       j++) // 头尾一一对称交换，i其实就是字符串的长度，索引最大值比长度少1
+  {
+    temp = str[j];               // 头部赋值给临时变量
+    str[j] = str[i - 1 + k - j]; // 尾部赋值给头部
+    str[i - 1 + k - j] = temp; // 将临时变量的值(其实就是之前的头部值)赋给尾部
+  }
+
+  return str; // 返回转换后的字符串
+}
+#endif
---
+++ quickjs-memorydump.h
@@ -0,0 +1,144 @@
+#ifndef QUICKJS_MEMORY_DUMP_H
+#define QUICKJS_MEMORY_DUMP_H
+#include "quickjs.h"
+#ifdef CONFIG_INTERPRETERS_QUICKJS_DEBUG
+#include "list.h"
+#include <stdint.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+#define INVALID_MEMORY_PTR 0
+#define CDP_SELF 0
+#define CDP_CHILD 1
+
+#define CDP_UNKNOW_DEFAULT_NAME ("anymouse")
+#define CDP_OBJECT_DEFAULT_NAME ("object")
+#define CDP_STRING_DEFAULT_NAME ("string")
+#define CDP_BYTECODE_DEFAULT_NAME ("file")
+#define CDP_SYMBOL_DEFAULT_NAME ("symbol")
+#define CDP_VALUE_DEFAULT_NAME ("value")
+#define CDP_VARREF_DEFAULT_NAME ("var_ref")
+#define CDP_VARREF_NATIVE_FINCTION_NAME ("Native function")
+#define CDP_UNDEFINED_NAME ("undefined")
+
+typedef int64_t memory_object_id;
+typedef int Self_or_child;
+
+#define IS_FREE_MASK 0x1
+#define GET_NEED_FREE(val) (val & IS_FREE_MASK)
+
+#define CLEAR_NEED_FREE(val) ((val) = (((val) >> 1) << 1))
+
+#define GET_LENGTH(val) (val >> 1)
+#define STORE_VAL(len, need_free) ((int)(len << 1 | need_free))
+typedef struct CDP_memory_str_val {
+  int flag;
+  const char *name;
+} CDP_memory_str_val;
+
+typedef enum EntryType {
+  EntryHidden = 0,        // Hidden node, may be filtered when shown to user.
+  EntryArray = 1,         // An array of elements.
+  EntryString = 2,        // A string.
+  EntryObject = 3,        // A JS object (except for arrays and strings).
+  EntryCode = 4,          // Compiled code.
+  EntryClosure = 5,       // Function closure.
+  EntryRegExp = 6,        // RegExp.
+  EntryHeapNumber = 7,    // Number stored in the heap.
+  EntryNative = 8,        // Native object (not from V8 heap).
+  EntrySynthetic = 9,     // Synthetic object, usually used for grouping
+                          // snapshot items together.
+  EntryConsString = 10,   // Concatenated string. A pair of pointers to strings.
+  EntrySlicedString = 11, // Sliced string. A fragment of another string.
+  EntrySymbol = 12,       // A Symbol (ES6).
+  EntryBigInt = 13,       // BigInt.
+  EntryObjectShape = 14,  // Internal data used for tracking the shapes (or
+                          // "hidden classes") of JS objects.
+} EntryType;
+
+typedef struct HeapUsage {
+  int64_t totalSize;
+  int64_t usedSize;
+  int64_t totalCount;
+} Macro_heap_info;
+
+typedef enum GraphType {
+  GraphContextVariable = 0, // A variable from a function context.
+  kElement = 1,             // An element of an array.
+  GraphProperty = 2,        // A named object property.
+  GraphInternal = 3,        // A link that can't be accessed from JS,
+                            // thus, its name isn't a real property name
+                            // (e.g. parts of a ConsString).
+  GraphHidden = 4,          // A link that is needed for proper sizes
+                            // calculation, but may be hidden from user.
+  GraphShortcut = 5,        // A link that must not be followed during
+                            // sizes calculation.
+  GraphWeak = 6             // A weak reference (ignored by the GC).
+} GraphType;
+
+typedef void (*CDP_add_memory_object)(JSRuntime *rt,
+                                      memory_object_id parent_ptr,
+                                      EntryType type, memory_object_id id,
+                                      CDP_memory_str_val *val, int64_t size,
+                                      CDP_memory_str_val *obj_name);
+
+typedef void (*CDP_add_memory_object_child_by_id)(
+    JSRuntime *rt, memory_object_id id, memory_object_id child,
+    CDP_memory_str_val *child_name);
+typedef void (*CDP_add_memory_object_size_by_id)(JSRuntime *rt,
+                                                 memory_object_id id,
+                                                 int64_t size);
+typedef void (*CDP_add_memory_object_value_by_id)(JSRuntime *rt,
+                                                  memory_object_id id,
+                                                  CDP_memory_str_val *val);
+typedef void (*CDP_add_memory_object_type_by_id)(JSRuntime *rt,
+                                                 memory_object_id id,
+                                                 EntryType type);
+typedef void (*CDP_add_memory_object_obj_name_by_id)(
+    JSRuntime *rt, memory_object_id id, CDP_memory_str_val *obj_name);
+
+typedef void (*CDP_GC_obj_change)(JSRuntime *rt, memory_object_id id,
+                                  int64_t count, int64_t size);
+
+typedef struct DumpMemoryInfo {
+  size_t is_started_memory_tracking;       /* Is heap object tracking enabled*/
+  int is_started_memory_tracking_on_timer; /* Track memory along a timeline */
+  CDP_add_memory_object add_memory_object;
+  CDP_add_memory_object_child_by_id add_memory_object_child_by_id;
+  CDP_add_memory_object_size_by_id add_memory_object_size_by_id;
+  CDP_add_memory_object_value_by_id add_memory_object_value_by_id;
+  CDP_add_memory_object_type_by_id add_memory_object_type_by_id;
+  CDP_add_memory_object_obj_name_by_id add_memory_object_obj_name_by_id;
+  CDP_GC_obj_change GC_obj_change;
+  memory_object_id currnt_val_id;
+} DumpMemoryInfo;
+
+typedef struct Child_info {
+  memory_object_id id;
+  CDP_memory_str_val child_name;
+} Child_info;
+
+void CDP_start_memory_tracking(JSRuntime *rt);
+void CDP_stop_memory_tracking(JSRuntime *rt);
+struct DumpMemoryInfo *getDumpMemoryInfo(JSRuntime *rt);
+Macro_heap_info CDP_get_heap_usage(JSRuntime *rt);
+void CDP_sacn_heap_in_memory(JSRuntime *rt);
+void CDP_get_stats_update_info(JSRuntime *rt, JSGCObjectHeader *h);
+void CDP_rm_GC_obj(JSRuntime *rt, JSGCObjectHeader *h);
+CDP_memory_str_val CDP_create_obj_name(const char *str, int len);
+CDP_memory_str_val CDP_get_obj_name(JSRuntime *rt, JSAtom atom);
+char *CDP_int_to_string(int num, char *str, int radix);
+memory_object_id CDP_get_val_id(JSRuntime *rt, const JSValueConst *val);
+// Insert children into the children of nodes in the proxy tree. These children
+// will be traversed in the subsequent gclist
+void CDP_add_proxies_obj_child(JSRuntime *rt, memory_object_id parent_id,
+                               memory_object_id child_id,
+                               CDP_memory_str_val *child_name);
+void CDP_get_GC_obj_count_and_size(JSRuntime *rt, JSGCObjectHeader *gp,
+                                   int64_t *count, int64_t *size);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+#endif
