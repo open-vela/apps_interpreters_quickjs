038028fed2f2adf8884922edd2e200ca4f73084e
yinshengkai <yinshengkai@xiaomi.com>
2022-12-02 21:44:46 +0800

    quickjs: enable object DUMP_LEAKS
    Signed-off-by: yinshengkai <yinshengkai@xiaomi.com>

--- quickjs.c
+++ quickjs.c
@@ -1977,7 +1977,6 @@ void JS_FreeRuntime(JSRuntime *rt)
 
     JS_RunGC(rt);
 
-#ifdef DUMP_LEAKS
     /* leaking objects */
     {
         BOOL header_done;
@@ -2015,8 +2014,6 @@ void JS_FreeRuntime(JSRuntime *rt)
         if (count != 0)
             printf("Secondary object leaks: %d\n", count);
     }
-#endif
-    assert(list_empty(&rt->gc_obj_list));
 
     /* free the classes */
     for(i = 0; i < rt->class_count; i++) {
